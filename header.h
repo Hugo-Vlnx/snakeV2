a57ce5a18b8d9bb16be4519067e8b3ee246d8c7c
skitMex <martin.hurvois@student.junia.com>
2025-01-12 22:26:52 +0100

    Update header.h

--- header.h
+++ header.h
@@ -7,9 +7,7 @@ void afficheGrille(char grille[46][46],int n );
 void initialisation(char grille[46][46], int n, int teteX, int teteY);
 void nourriture(char grille[46][46]);
 
-int deplacementSnake(char grille[46][46], int n, int teteX, int teteY, int dirX, int dirY, int longueur); // il manque les commandes pour actuellement deplacer le snake -hugo
-
-int deplacerSnake(char grille[46][46], int n, int teteX, int teteY, int longueur);
+int deplacementSnake(char grille[46][46], int n, int teteX, int teteY, int dirX, int dirY, int longueur); // commande de deplacement dans le main -martin
 
 int verificationManger(char grille[46][46], int n, int teteX, int teteY, int longueur);
 int verificationCollision(char grille[46][46], int n, int teteX, int teteY, int longueur);
