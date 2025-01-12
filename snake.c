9001696064a97f633ea5a4d4d97001b2ae848feb
skitMex <martin.hurvois@student.junia.com>
2025-01-12 21:45:27 +0100

    Update snake.c

--- snake.c
+++ snake.c
@@ -13,7 +13,11 @@ int main() {
 	char grille[46][46];
 
 	int n = 46;
+	int teteX, teteY;
+	int dirX = 0;
+	dirY = 1; 
 	int longueur = 3;
+	int jeuEnCours = 1;
 	for (int i = 0; i < n; i++) {
 		for (int j = 0; j < n; j++) {
 			grille[i][j] = ' ';
